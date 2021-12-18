861634ad16474b3c4f3820bab3e6a4b32caec993
CarlbertCannon <providenceneedschange@gmail.com>
2021-12-18 10:32:04 -0700

    Intialized changes

---
+++ IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BestBuyBestPractices
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetDepartments();
+        void InsertDepartment(string newDepartmentName);
+    }
+}
